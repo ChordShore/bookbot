6cb461aaea3b1d628e62cc72d310a4c4eb3994de
ChordShore <contact@chordshore.com>
2025-05-07 16:00:24 -0700

    Added word cound logic.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def get_book_text(file_path):
 def main():
     file_path = "./books/frankenstein.txt"
     file_contents = get_book_text(file_path)
-    print(file_contents)
+    num_words = len(file_contents.split())
+    print(f"'{num_words} words found in the document'")
 
 main()
