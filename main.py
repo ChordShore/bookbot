6e1241173221a8d589587d869d2708bdfd053016
ChordShore <contact@chordshore.com>
2025-05-07 15:05:28 -0700

    Successfully wrote file import & read logic.

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(file_path):
+    with open(file_path) as f:
+        contents = f.read()
+        return contents
+
+def main():
+    file_path = "./books/frankenstein.txt"
+    file_contents = get_book_text(file_path)
+    print(file_contents)
+
+main()
