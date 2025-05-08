725bbb893bdb3c37ec0768db17e0c901b215b4a0
ChordShore <contact@chordshore.com>
2025-05-08 15:23:33 -0700

    Created character count function.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words
+from stats import get_num_chars
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -9,6 +10,8 @@ def main():
     file_path = "./books/frankenstein.txt"
     file_contents = get_book_text(file_path)
     num_words = get_num_words(file_contents)
+    num_chars = get_num_chars(file_contents)
     print(f"'{num_words} words found in the document'")
+    print(num_chars)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,22 @@
 def get_num_words(file_contents):
     num_words = len(file_contents.split())
     return num_words
+
+def get_num_chars(file_contents):
+    num_chars = {}
+    
+    for char in range(0, len(file_contents)):
+        converted_char = file_contents[char].lower()
+        
+        if num_chars == {}:
+            num_chars[converted_char] = 1
+        elif converted_char not in num_chars:
+            num_chars[converted_char] = 1
+        else:
+            num_chars[converted_char] += 1
+        
+    #print(num_chars['t'])
+    #print(num_chars['p'])
+    #print(num_chars['c'])
+
+    return num_chars
