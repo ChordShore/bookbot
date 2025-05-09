f5716f44038a12341ceb5ea29e057a84324e5589
ChordShore <contact@chordshore.com>
2025-05-09 13:04:06 -0700

    Added sorting & print formatting.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import get_num_words
 from stats import get_num_chars
+from stats import get_sorted_list
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -11,7 +12,16 @@ def main():
     file_contents = get_book_text(file_path)
     num_words = get_num_words(file_contents)
     num_chars = get_num_chars(file_contents)
-    print(f"'{num_words} words found in the document'")
-    print(num_chars)
+    sorted_list = get_sorted_list(num_chars)
+
+    #Print Formatting
+    print("♢♢♢♢♢ BOOKBOT ♢♢♢♢♢")
+    print(f"Analyzing book found at {file_path}...")
+    print("⋄⋄⋄⋄⋄ Word Count ⋄⋄⋄⋄⋄")
+    print(f"Found {num_words} total words")
+    print("⋄⋄⋄⋄⋄ Character Count ⋄⋄⋄⋄⋄")
+    for i in range(0, len(sorted_list)):
+        print(f"{sorted_list[i]["char"]}: {sorted_list[i]["num"]}")
+    print("⋄⋄⋄⋄⋄ END ⋄⋄⋄⋄⋄")
 
 main()
--- stats.py
+++ stats.py
@@ -3,20 +3,24 @@ def get_num_words(file_contents):
     return num_words
 
 def get_num_chars(file_contents):
-    num_chars = {}
-    
+    num_chars = {}   
     for char in range(0, len(file_contents)):
         converted_char = file_contents[char].lower()
-        
         if num_chars == {}:
             num_chars[converted_char] = 1
         elif converted_char not in num_chars:
             num_chars[converted_char] = 1
         else:
-            num_chars[converted_char] += 1
-        
-    #print(num_chars['t'])
-    #print(num_chars['p'])
-    #print(num_chars['c'])
-
+            num_chars[converted_char] += 1    
     return num_chars
+
+def sort_on(dict):
+    return dict["num"]
+
+def get_sorted_list(num_chars):
+    sorted_list = []
+    for char in num_chars:
+        if char.isalpha() == True:
+            sorted_list.append({"char": char, "num": num_chars[char]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
